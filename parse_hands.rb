891cf6239e942185355fab5dfa19b3f580fcc673
Robert Kaufman <rob.m.kaufman@gmail.com>
2015-11-28 21:54:53 -0500

    Clean up, remove UTF-16 byte order mark

--- parse_hands.rb
+++ parse_hands.rb
@@ -2,15 +2,32 @@ require 'ostruct'
 
 @hands = []
 
-def parse_file(filename)
-  File.read(filename).split("\r\n").each do |line|
+def parse_hand(hand_data)
+  hand = OpenStruct.new
+
+  hand_data.each do |line|
     if line =~ /\ABovada Hand #/
-      hand = OpenStruct.new
       hand.id, hand.table_id, hand.time =
         line.match(/#(\d+) TBL#(\d+).*(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})/).captures
-
-      @hands << hand
     end
+  end
+
+  @hands << hand
+end
+
+def parse_file(filename)
+  hand_data = []
+
+  File.read(filename).split("\r\n").each do |line|
+    # Remove UTF-16 byte order mark
+    line.sub!(/\ufeff/, '')
+
+    if hand_data.length > 0 && line =~ /\ABovada Hand #/
+      parse_hand(hand_data)
+      hand_data = []
+    end
+
+    hand_data << line
   end
 end
 
