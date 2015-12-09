c32abde7df94c6b3c9c73f6c47ac515d1fd93f83
Robert Kaufman <rob.m.kaufman@gmail.com>
2015-12-09 18:07:07 -0500

    Calculate cells

--- replayer.rb
+++ replayer.rb
@@ -3,6 +3,27 @@ include Curses
 
 init_screen
 
+# The entire screen is filled up by cells. A cell is determined by:
+# x: Column offset from left.
+# y: Row offset from TOP.
+
+# All cells have the same height and width
+
+cell_width = cols / 4
+cell_height = lines / 3
+
+cells = 3.times.collect do |row_index|
+  4.times.collect do |column_index|
+    {
+      x: cell_width * column_index,
+      y: cell_height * row_index
+    }
+  end
+end
+
+puts cells.inspect
+exit
+
 player_positions = {
   'Player 1' => [5, (cols / 2) - 8]
 }
